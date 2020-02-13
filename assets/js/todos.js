0293bb9a10657d548c3acace2dbbed8644d895f4
yune <annieeun@kaist.ac.kr>
2020-02-13 16:00:34 +0900

    feat: Add deleting todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,11 @@ $("li").click(function() {
 	//if li is gray
 	$(this).toggleClass("completed");
 });
+
+//click on x tot delete todo
+$("span").click(function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
